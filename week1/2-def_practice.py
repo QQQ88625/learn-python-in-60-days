4914e72e261d39da757cda307275c298ee7ec989
qqq88625 <wsx199813@gmail.com>
2022-03-31 21:48:18 +0800

    remove redundant spaces

--- week1/2-def_practice.py
+++ week1/2-def_practice.py
@@ -7,11 +7,12 @@ def calculate_number(first):
         total += first
         # 表示total=total +first 
         print (total)
-        
+
 def app_log(name,func_id):
     print(f"{name} use the {func_id}")
     #函數裡的的參數，可以藉由執行自行做更換
-    
+
+
 def app_log(name,func_id,date="2022/03/29"):
     print(f"{name} use the {func_id} on {date}")
     #一開始參數設定時，我們可以先設一個數值。如果後面使用這個參數時，沒有特別指定，系統就會使用預設值
