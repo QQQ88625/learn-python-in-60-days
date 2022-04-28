f4c1eb575ea71170db373fd461be93cb5a212e4f
Demonic <demonic@csie.io>
2022-04-28 21:09:07 +0800

    Create 6-HW5-dict-practice.py

---
+++ Ian chi HW series/6-HW5-dict-practice.py
@@ -0,0 +1,18 @@
+x = {"name": "Peter", "money":1000 }
+
+# expected outpot:Peter
+print(x['name'])
+print(x['money'])
+
+
+x = {"name": "Peter", "money":1000 }
+for key in x:
+    print(key)
+ 
+    print(x[key])
+    
+for value in x:
+    print(value)
+ 
+    print(x[value])    
+    
